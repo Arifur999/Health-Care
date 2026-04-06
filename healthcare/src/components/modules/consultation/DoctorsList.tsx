cd07208fd4c62e62ef41f3965be61bcc2c8a30e9
Arifur999 <arr96777@gmail.com>
2026-04-06 23:21:32 +0600

    added fatching data

--- healthcare/src/components/modules/consultation/DoctorsList.tsx
+++ healthcare/src/components/modules/consultation/DoctorsList.tsx
@@ -1,6 +1,22 @@
+"use client";
+import { getDoctors } from "@/app/(commonLayout)/consultation/_actions";
+import { useQuery } from "@tanstack/react-query";
+
 const DoctorsList=()=> {
+    const { data } = useQuery({
+        queryKey: ["doctors"],
+        queryFn: () => getDoctors(),
+          // Simulate an API call
+      });
+
+      console.log(data);
   return (
-    <div>page</div>
+    <div>
+      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+      {data.data?.map((doctor:any) => (
+        <div key={doctor.id}>{doctor.name}</div>
+      ))}
+    </div>
   )
 }
 
