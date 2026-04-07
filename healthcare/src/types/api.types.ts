bdeb1686f394224047e1a9708a4cfe8e8d1f3cf6
Arifur999 <arr96777@gmail.com>
2026-04-07 23:02:06 +0600

    added api interface

---
+++ healthcare/src/types/api.types.ts
@@ -0,0 +1,21 @@
+export interface ApiResponse<TData = unknown> {
+    success: boolean;
+    message: string;
+    data: TData;
+    meta?: PaginationMeta;
+    status: number;
+   
+}
+
+export interface PaginationMeta{
+    page:number;
+    limit:number;
+    total:number;
+    totalPages:number;
+}
+
+export interface ApiErrorResponse {
+    success: boolean;
+    message: string;
+  \
+}
