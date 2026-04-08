e86301bbfc38028c141ff6d459021ac37f0d6894
Arifur999 <arr96777@gmail.com>
2026-04-08 23:43:36 +0600

    added cookies setup

--- healthcare/src/app/(commonLayout)/(auth)/login/_action.ts
+++ healthcare/src/app/(commonLayout)/(auth)/login/_action.ts
@@ -1,8 +1,10 @@
 "use server";
 import { httpClient } from "@/lib/axios/httpClient";
+import { setTokenInCookies } from "@/lib/tokenUtils";
 import { ApiErrorResponse } from "@/types/api.types";
 import { ILoginResponse } from "@/types/auth.types";
 import { ILogin, LoginZodSchema } from "@/zod/auth.validation";
+import { redirect } from "next/navigation";
 
 
 
@@ -21,10 +23,14 @@ export const LoginAction = async (payload : ILogin): Promise<ILoginResponse | Ap
     try {
         
         const response = await httpClient.post<ILoginResponse>("/auth/login", parsedPayload.data);
-        const { token, accessToken, refreshToken, user } = response.data;
+        const { token, accessToken, refreshToken } = response.data;
         // Store tokens in cookies
+    
+        await setTokenInCookies("accessToken", accessToken);
+        await setTokenInCookies("refreshToken", refreshToken);
+        await setTokenInCookies("better-auth.session_token", token);
        
-        return response.data;
+        redirect("/dashboard");
 
     } catch (error) {
         return {
