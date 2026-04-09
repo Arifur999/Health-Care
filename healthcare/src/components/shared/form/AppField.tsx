58a0b64e8b62a27ba3376a5265e8aac66e2e5036
Arifur999 <arr96777@gmail.com>
2026-04-09 23:24:27 +0600

    added AppField append prepend

--- healthcare/src/components/shared/form/AppField.tsx
+++ healthcare/src/components/shared/form/AppField.tsx
@@ -1,3 +1,5 @@
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
 import { AnyFieldApi } from '@tanstack/react-form'
 
 
@@ -11,5 +13,58 @@ type AppFieldProps = {
  prepend?:string;
  className?:string;
  disabled?:boolean;
+
+}
+
+const AppField = ({
+    field,
+    label,
+    type="text",
+    placeholder,
+    append,
+    prepend,
+    className,
+    disabled=false
+}:AppFieldProps) => {
+    return (
+        <div className={cn("space-y-1.5",className)}>
+           <Label 
+              htmlFor={field.name}
+              className={cn("font-medium", disabled && "cursor-not-allowed opacity-50")}
+              >{label}</Label>
+              <div className="relative">
+                {
+                  prepend && (<div className="absolute inset-y-0 left-0 items-center pl-3 pointer-events-none z-10">
+                    {prepend}
+                  </div>)
+                
+                }
+
+                <input
+                  id={field.name}
+                  name={field.name}
+                  type={type}
+                  value={field.state.value}
+                  placeholder={placeholder}
+                  onBlur={field.handleBlur}
+                  onChange={(e)=>field.handleChange(e.target.value)}
+                  disabled={disabled}
+                //   aria-invalid={field.state.error ? "true" : "false"}
+                // aria-describedby=''
+                  className={cn(
+                      prepend && "pl-10" ,
+                      append && "pr-10" ,
+                    
+                  )}
+                 
+                />
+                {append && (<div className="absolute inset-y-0 right-0 items-center pr-3 pointer-events-none z-10">
+                    {append}
+                  </div>)
+                }
+
+                </div>
+        </div>
+    )
 
 }
