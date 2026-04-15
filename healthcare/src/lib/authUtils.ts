1779be25896a95de28488c431dd4383d4b8bd544
Arifur999 <arr96777@gmail.com>
2026-04-15 19:38:57 +0600

    added route matcher

--- healthcare/src/lib/authUtils.ts
+++ healthcare/src/lib/authUtils.ts
@@ -6,3 +6,28 @@ export const authRoles =["/login","/register","/verify-email","/forgot-password"
     export const isAuthRoute = (pathname:string) => {
         return authRoles.some((router :string) => router === pathname);
     }
+
+export type RouteConfig = {
+    exact:string[];
+    pattern:RegExp[];
+}
+
+export const commonProtectedRoutes:RouteConfig = {
+    exact:["/my-profile","/change-password"],
+    pattern:[]
+}
+
+export const doctorProtectedRoutes:RouteConfig = {
+    pattern:[/^\/doctor\/dashboard/],
+    exact:[]
+}
+
+export const adminOrSuperAdminProtectedRoutes:RouteConfig = {
+    pattern:[/^\/admin\/dashboard/],
+    exact:[]
+}
+
+export const patientProtectedRoutes:RouteConfig = {
+    pattern:[/^\/dashboard/],
+    exact:["/payment/success"]
+}
