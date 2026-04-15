ef74c5e0b7680d39e217867893163e75a236be99
Arifur999 <arr96777@gmail.com>
2026-04-15 19:26:15 +0600

    added auth Roles proxy

--- healthcare/src/lib/authUtils.ts
+++ healthcare/src/lib/authUtils.ts
@@ -1,4 +1,8 @@
 export type UserRole = "SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT";
 
-export const authRoles =["/login","/register","/verify-email","/forgot-password",
-    "/reset-password"];
+export const authRoles =["/login","/register","/verify-email","/forgot-password","/reset-password"];
+    
+
+    export const isAuthRoute = (pathname:string) => {
+        return authRoles.some((router :string) => router === pathname);
+    }
