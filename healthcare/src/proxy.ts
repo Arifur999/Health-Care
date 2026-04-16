3feaf2b4f2d93e3b9b3389837fef7b0813eb7a2c
Arifur999 <arr96777@gmail.com>
2026-04-16 23:42:54 +0600

    added jwt utils setup

--- healthcare/src/proxy.ts
+++ healthcare/src/proxy.ts
@@ -1,14 +1,22 @@
 import { NextRequest } from "next/server";
 import { jwtUtils } from "./lib/jwtUtils";
+import { UserRole } from "./lib/authUtils";
 
 export async function proxy(request: NextRequest) {
-    const { pathname} =request.nextUrl;
+   try {
+     const { pathname} =request.nextUrl;
     const accessToken = request.cookies.get("accessToken")?.value;
     const refreshToken = request.cookies.get("refreshToken")?.value;
     
+    const decodedAccessToken= accessToken && jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string).decoded;
 
-    const isValidAccessToken = accessToken && jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string).success;
+    const isValidAccessToken=accessToken &&  jwtUtils.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string).success;
 
+    const userRole : UserRole | null;
+   
+   } catch (error) {
+    console.error("Error in proxy middleware:", error);
+   }
 }
 
 export const config = {
