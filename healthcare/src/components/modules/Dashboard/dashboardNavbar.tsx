4d173db66922ef2b59e618aa56c7d89113e338d8
Arifur999 <arr96777@gmail.com>
2026-04-27 20:42:50 +0600

    added dashboard navber

--- healthcare/src/components/modules/Dashboard/dashboardNavbar.tsx
+++ healthcare/src/components/modules/Dashboard/dashboardNavbar.tsx
@@ -1,9 +1,16 @@
-const DashboardNavbar = ()=> {
+import { getDefaultDashboardRoute } from "@/lib/authUtils"
+import { getNavItemsByRole } from "@/lib/navItem"
+import { getUserInfo } from "@/services/auth.services"
+import { NavSection } from "@/types/dashboard.types"
+
+const DashboardNavbar = async () => {
+   const userInfo = await getUserInfo()
+    const navItems : NavSection[] = getNavItemsByRole(userInfo.role)
+  
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role)
   return (
-    <div>
-    dashboard navbar
-    </div>
+    <DashboardNavbarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome}/>
   )
 }
 
-export default DashboardNavbar;
+export default DashboardNavbar
