30bd12bce95191db550dd73127df3f65880c1dcd
Arifur999 <arr96777@gmail.com>
2026-04-27 20:40:50 +0600

    added dashboard sidebar complete

--- healthcare/src/components/modules/Dashboard/dashboardSidebar.tsx
+++ healthcare/src/components/modules/Dashboard/dashboardSidebar.tsx
@@ -1,18 +1,18 @@
-import { getDefaultDashboardRoute } from "@/lib/authUtils";
-import { getNavItemsByRole } from "@/lib/navItem";
-import { getUserInfo } from "@/services/auth.services";
-import { NavSection } from "@/types/dashboard.types";
+import { getDefaultDashboardRoute } from "@/lib/authUtils"
+import { getNavItemsByRole } from "@/lib/navItem"
+import { getUserInfo } from "@/services/auth.services"
+import { NavSection } from "@/types/dashboard.types"
+import DashboardSidebarContent from "./dashboardSidebarContent"
 
-const DashboardSidebar = async()=> {
-  const userInfo=await getUserInfo();
-  const navItems: NavSection[]=getNavItemsByRole(userInfo.role)
 
-  const dashboardHome=getDefaultDashboardRoute(userInfo.role);
+const DashboardSidebar = async () => {
+  const userInfo = await getUserInfo()
+  const navItems : NavSection[] = getNavItemsByRole(userInfo.role)
+
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role)
   return (
-    <div>
-    dashboard sidebar
-    </div>
+    <DashboardSidebarContent userInfo={userInfo} navItems={navItems} dashboardHome={dashboardHome}/>
   )
 }
 
-export default DashboardSidebar;
+export default DashboardSidebar
