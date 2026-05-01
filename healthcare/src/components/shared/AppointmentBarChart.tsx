5f5999a57a967cce8d1124fad9af46b4c0f3bdcd
Arifur999 <arr96777@gmail.com>
2026-05-01 21:12:50 +0600

    added Appointment Bar Char with condition

--- healthcare/src/components/shared/AppointmentBarChart.tsx
+++ healthcare/src/components/shared/AppointmentBarChart.tsx
@@ -1,10 +1,29 @@
 import { BarChartData } from "@/types/dashboard.types";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 
 interface AppointmentBarChartProps {
     data : BarChartData[]
 }
 
 const AppointmentBarChart = ({data}: AppointmentBarChartProps) => {
+
+        if(!data || !Array.isArray(data)){
+        return (
+            <Card className="col-span-4">
+                <CardHeader>
+                    <CardTitle>Appointment Trends</CardTitle>
+                    <CardDescription>Monthly Appointment Statistics</CardDescription>
+                </CardHeader>
+                <CardContent className="flex items-center justify-center h-75">
+                    <p className="text-sm text-muted-foreground">
+                        Invalid data provided for the chart.
+                    </p>
+                </CardContent>
+            </Card>
+        )
+    }
+
+
     return (
         <div>   </div>
     )
