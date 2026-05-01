e5ff79444c5f7d445665a4c1483dab34951319c8
Arifur999 <arr96777@gmail.com>
2026-05-01 21:11:18 +0600

    added Appointment Bar Char

---
+++ healthcare/src/components/shared/AppointmentBarChart.tsx
@@ -0,0 +1,13 @@
+import { BarChartData } from "@/types/dashboard.types";
+
+interface AppointmentBarChartProps {
+    data : BarChartData[]
+}
+
+const AppointmentBarChart = ({data}: AppointmentBarChartProps) => {
+    return (
+        <div>   </div>
+    )
+}
+
+export default AppointmentBarChart
