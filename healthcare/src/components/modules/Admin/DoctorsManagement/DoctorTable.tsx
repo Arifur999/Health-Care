2d9e3d555b05eeb87f77a9c28209cc7dd224a1b1
Arifur999 <arr96777@gmail.com>
2026-05-05 21:21:21 +0600

    added doctors table error fix

--- healthcare/src/components/modules/Admin/DoctorsManagement/DoctorTable.tsx
+++ healthcare/src/components/modules/Admin/DoctorsManagement/DoctorTable.tsx
@@ -1,83 +1,67 @@
 "use client";
 
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { getDoctors } from "@/services/doctor.services";
 import { IDoctor } from "@/types/doctor.types";
 import { useQuery } from "@tanstack/react-query";
+import {
+    flexRender,
+    getCoreRowModel,
+    useReactTable
+} from "@tanstack/react-table";
 
 const DoctorTable = () => {
 
-    // const doctorColumns : ColumnDef<IDoctor>[] = [
-    //   { accessorKey: "name", header: "Name"},
-    // //   { accessorKey: "specialization", header: "Specialization" },
-    //   { accessorKey: "experience", header: "Experience" },
-    // //   { accessorKey: "rating", header: "Rating" },
-    // ];
+    const doctorColumns = [
+      { accessorKey: "name", header: "Name" },
+    //   { accessorKey: "specialization", header: "Specialization" },
+      { accessorKey: "experience", header: "Experience" },
+    //   { accessorKey: "rating", header: "Rating" },
+    ];
 
    
 
-    const { data : doctorDataResponse, isLoading } = useQuery({
+    const { data: doctors = [] } = useQuery<IDoctor[]>({
         queryKey: ["doctors"],
         queryFn: getDoctors
     });
 
-    const { data : doctors } = doctorDataResponse! || [];
-
-    const handleView = (doctor : IDoctor) => {
-        console.log("View doctor", doctor);
-    }
-
-    const handleEdit = (doctor : IDoctor) => {
-        console.log("Edit doctor", doctor);
-    }
-
-    const handleDelete = (doctor : IDoctor) => {
-        console.log("Delete doctor", doctor);
-    }
-
-
-    // const { getHeaderGroups, getRowModel } = useReactTable({
-    //    data: doctors,
-    //    columns: doctorColumns,
-    //    getCoreRowModel: getCoreRowModel(),
-    // });   
-
-    // console.log(doctorDataResponse?.data.map(doctor => doctor.name));
+    const { getHeaderGroups, getRowModel } = useReactTable({
+       data: doctors,
+       columns: doctorColumns,
+       getCoreRowModel: getCoreRowModel(),
+    });   
 
     console.log(doctors);
-  // return (
-  //   <Table>
-  //     <TableHeader>
-  //       {getHeaderGroups().map((hg) => (
-  //         <TableRow key={hg.id}>
-  //           {hg.headers.map((header) => (
-  //             <TableHead key={header.id}>
-  //               {flexRender(
-  //                 header.column.columnDef.header,
-  //                 header.getContext(),
-  //               )}
-  //             </TableHead>
-  //           ))}
-  //         </TableRow>
-  //       ))}
-  //     </TableHeader>
-  //     <TableBody>
-  //       {getRowModel().rows.map((row) => (
-  //         <TableRow key={row.id}>
-  //           {row.getVisibleCells().map((cell) => (
-  //             <TableCell key={cell.id}>
-  //               {flexRender(cell.column.columnDef.cell, cell.getContext())}
-  //             </TableCell>
-  //           ))}
-  //         </TableRow>
-  //       ))}
-  //     </TableBody>
-  //   </Table>
-  // );
-
-    return (
-      <></>
-    )
-
+  return (
+    <Table>
+      <TableHeader>
+        {getHeaderGroups().map((hg) => (
+          <TableRow key={hg.id}>
+            {hg.headers.map((header) => (
+              <TableHead key={header.id}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
+              </TableHead>
+            ))}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {getRowModel().rows.map((row) => (
+          <TableRow key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <TableCell key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
 
 export default DoctorTable
