053d0c52454ba981e3d0102a26fd05f03130cbad
Arifur999 <arr96777@gmail.com>
2026-06-10 22:15:34 +0600

    add sarch query

--- healthcare/src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
+++ healthcare/src/app/(dashboardLayout)/admin/dashboard/doctors-management/page.tsx
@@ -1,8 +1,20 @@
 import DoctorTable from "@/components/modules/Admin/DoctorsManagement/DoctorTable";
 import { getDoctors } from "@/services/doctor.services";
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import {use} from "react";
 
-const DoctorsManagementPage=async ()=> {
+const DoctorsManagementPage=async ({ 
+  searchParams,
+}:{
+  searchParams:  Promise<{ [key: string]: string | string[] | undefined }>;
+})=> {
+  const queryParamsObject = await searchParams;
+  console.log(queryParamsObject);
+
+  const queryString = Object.entries(queryParamsObject)
+    .map(([key, value]) => `${key}=${Array.isArray(value) ? value.join(",") : value ?? ""}`)
+    .join("&");
+
 
    const queryClient = new QueryClient();
 
